72c128aedbcd12ab4c87e3094edb8e6b5655faf2
Adylbek Dzhoroev <adikjoro@gmail.com>
2022-06-15 18:08:38 +0600

    add home section content

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,28 @@
 function App() {
   return (
     <div className="App">
-      <header>
-        
-      </header>
+      <header></header>
       <section className="main">
-
+        <div className="content">
+          <div className="grid content-center">
+            <div className="bg-white py-3.5 pl-[18px] pr-[7px]">
+              <h2 className="font-bold text-3.5xl text-primary">
+                A Great App Makes Your Life Better
+              </h2>
+            </div>
+            <div className="absolute h-[120%]" />
+          </div>
+        </div>
       </section>
-      <section className="about">
-
-      </section>
-      <section className="features">
-
-      </section>
-      <section className="checkout">
-
-      </section>
-      <section className="download">
-
-      </section>
-      <section className="manual">
-
-      </section>
-      <section className="team">
-
-      </section>
-      <section className="customers">
-
-      </section>
+      <section className="about"></section>
+      <section className="features"></section>
+      <section className="checkout"></section>
+      <section className="download"></section>
+      <section className="manual"></section>
+      <section className="team"></section>
+      <section className="customers"></section>
       <section className="blog"></section>
-      <footer>
-
-      </footer>
+      <footer></footer>
     </div>
   );
 }
