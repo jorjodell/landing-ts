7752c40b3a32f14cc68ec584f3c880281d506bcf
Adylbek Dzhoroev <adikjoro@gmail.com>
2022-06-15 17:35:50 +0600

    create base layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,37 @@
 function App() {
   return (
     <div className="App">
-      Landing typescript
+      <header>
+        
+      </header>
+      <section className="main">
+
+      </section>
+      <section className="about">
+
+      </section>
+      <section className="features">
+
+      </section>
+      <section className="checkout">
+
+      </section>
+      <section className="download">
+
+      </section>
+      <section className="manual">
+
+      </section>
+      <section className="team">
+
+      </section>
+      <section className="customers">
+
+      </section>
+      <section className="blog"></section>
+      <footer>
+
+      </footer>
     </div>
   );
 }
